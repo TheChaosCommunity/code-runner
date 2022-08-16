ac87bb20765c59d7a2e0c51a0e9568a15ab00e6c
UniqueName12345 <88826851+UniqueName12345@users.noreply.github.com>
2022-08-16 11:07:44 +0800

    Change Branding and Remove Unnessecary Stuff

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -7,12 +7,8 @@
     class="text-gray-900 mx-auto relative flex justify-between align-middle w-full bg-sky-500 min-h-50 px-4 py-2"
   >
     <a href="{base}/" class="flex justify-start w-fit"
-      ><h1 class="text-2xl font-bold">Coding Challenge</h1></a
+      ><h1 class="text-2xl font-bold">Code Runner</h1></a
     >
-    <ul class="flex float-right items-center space-x-2">
-      <a href="{base}/scoreboard"
-        ><li class="ml-2 text-base hover:font-semibold transition-all">Scoreboard</li></a
-      >
       <a href="{base}/run"
         ><li class="ml-2 text-base hover:font-semibold transition-all">Run JavaScript D8</li></a
       >
